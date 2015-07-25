fb441d21302583cdea2a05cea37af7a26f1ad7b2
htmlguy <htmlguy999@gmail.com>
2015-07-25 12:39:41 -0400

    Recompile

--- build/cftr.js
+++ build/cftr.js
@@ -35,7 +35,7 @@ Artist = (function() {
       this.ctx.save();
       this.ctx.scale(-1, 0);
     }
-    this.ctx.drawImage(image, x, y);
+    this.ctx.drawImage(this.game.gameMediaManager.images[image], x, y);
     if (flip) {
       return this.ctx.restore();
     }
@@ -81,11 +81,14 @@ Game = (function() {
     } else {
       throw new GameInitializationError("The canvas was not created.");
     }
-    this.gameMediaManager = new GameMediaManager(this);
-    this.gameLevelManager = new GameLevelManager(this);
-    this.gameRenderer = new GameRenderer(this);
-    this.gameObjectManager = new GameObjectManager(this);
-    this.startLoop();
+    this.gameMediaManager = new GameMediaManager(this(function() {
+      return this.gameLevelManager = new GameLevelManager(this(function() {
+        this.gameRenderer = new GameRenderer(this);
+        this.gameKeybindingManager = new GameKeybindingManager(this);
+        this.gameLevelManager.levels[this.level].initialize();
+        return this.startLoop();
+      }));
+    }));
   }
 
   Game.prototype.initCanvas = function() {
@@ -107,12 +110,35 @@ Game = (function() {
   Game.prototype.animationFrame = function() {
     this.loops = 0;
     while (new Date().getTime() > this.nextTick && this.loops < this.maxSkip) {
-      this.gameObjectManager.update();
+      this.gameLeveManager.levels[this.level].update();
       this.nextTick += this.skipTicks;
       this.loops++;
     }
     this.gameRenderer.render();
+    if (this.gameLevelManager.levels[this.level].music) {
+      this.gameMediaManager.audio[this.gameLevelManager.levels[this.level].music].play();
+    }
     return requestAnimationFrame(this.animationFrame);
+  };
+
+  Game.prototype.playAudio = function(aud) {
+    return this.gameMediaManager.audio[aud].play();
+  };
+
+  Game.prototype.changeLevels = function(n) {
+    this.stopMusic();
+    this.level = n;
+    return this.gameLevelManager.levels[n].initialize();
+  };
+
+  Game.prototype.stopMusic = function() {
+    var i, results;
+    results = [];
+    for (i in this.gameMediaManager.audio) {
+      this.gameMediaMAnager.audio[i].pause();
+      results.push(this.gameMediaMAnager.audio[i].currentTime = 0);
+    }
+    return results;
   };
 
   Game.prototype.name = "The game";
@@ -134,6 +160,10 @@ Game = (function() {
   Game.prototype.imagesNeeded = [];
 
   Game.prototype.levelsNeeded = [];
+
+  Game.prototype.level = 0;
+
+  Game.prototype.keybindings = {};
 
   return Game;
 
@@ -190,3 +220,182 @@ GameInitializationError = (function(superClass) {
   return GameInitializationError;
 
 })(GameError);
+
+
+/*
+  The Calefactor Engine
+  Copyright (C) 2015 Calefactor Contributors
+
+  This file is part of The Calefactor Engine.
+
+  The Calefactor Engine is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The Calefactor Engine is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with The Calefactor Engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var GameRenderer;
+
+GameRenderer = (function() {
+  function GameRenderer(game) {
+    this.game = game;
+  }
+
+  GameRenderer.prototype.render = function() {
+    var curobjs, i, j, len, results;
+    this.game.artist.clear();
+    curobjs = this.game.gameLevelManager.levels[this.game.level].objects;
+    results = [];
+    for (j = 0, len = curobjs.length; j < len; j++) {
+      i = curobjs[j];
+      if (curobjs[i].image) {
+        this.game.artist.image(curobjs[i].image, curobjs[i].x, curobjs[i].y, curobjs[i].flipImage);
+      }
+      if (curobjs[i].render) {
+        results.push(curobjs[i].render());
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  };
+
+  return GameRenderer;
+
+})();
+
+
+/*
+  The Calefactor Engine
+  Copyright (C) 2015 Calefactor Contributors
+
+  This file is part of The Calefactor Engine.
+
+  The Calefactor Engine is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The Calefactor Engine is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with The Calefactor Engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Level;
+
+Level = (function() {
+  function Level(game, url, finished) {
+    var request, that;
+    this.game = game;
+    that = this;
+    request = new XMLHttpRequest;
+    request.onload = function() {
+      return that.parse(this.responseText(finished));
+    };
+    request.open("get", url, true);
+    request.send();
+  }
+
+  Level.prototype.parse = function(json, finished) {
+    this.jsondata = JSON.parse(json);
+    this.name = this.jsondata.name;
+    this.music = this.jsondata.music;
+    return finished();
+  };
+
+  Level.prototype.initialize = function() {
+    var i, k, len, ref, results, separated;
+    this.objects = [];
+    ref = this.jsondata.objects;
+    results = [];
+    for (k = 0, len = ref.length; k < len; k++) {
+      i = ref[k];
+      separated = this.jsondata.objects[i].split(":");
+      results.push(this.objects[i] = new window[separated[0] + "Object"](separated[1], separated[2], separated[3]));
+    }
+    return results;
+  };
+
+  Level.prototype.update = function() {
+    var collisions, i, j, k, l, len, len1, ref, ref1, results;
+    ref = this.objects;
+    results = [];
+    for (k = 0, len = ref.length; k < len; k++) {
+      i = ref[k];
+
+      /* Surrounds the collison detection as well because collision detection
+      is pointless without an object update.
+       */
+      if (this.objects[i].update) {
+        collisions = [];
+        ref1 = this.objects;
+        for (l = 0, len1 = ref1.length; l < len1; l++) {
+          j = ref1[l];
+          if (this.checkIntersections(this.objects[i], this.objects[j])) {
+            collisons.push(j);
+          }
+        }
+        results.push(this.objects[i].update(collisions));
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  };
+
+  Level.prototype.checkIntersections = function(a, b) {
+    return !(b.x > a.x + a.width || b.x + b.width < a.x || b.y + b.height > a.y || b.y < a.y + a.height);
+  };
+
+  return Level;
+
+})();
+
+
+/*
+  The Calefactor Engine
+  Copyright (C) 2015 Calefactor Contributors
+
+  This file is part of The Calefactor Engine.
+
+  The Calefactor Engine is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The Calefactor Engine is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with The Calefactor Engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Object;
+
+Object = (function() {
+  function Object(game, x, y, datavalue) {
+    this.game = game;
+    this.x = x != null ? x : 0;
+    this.y = y != null ? y : 0;
+    this.datavalue = datavalue != null ? datavalue : 0;
+    this.initialize();
+  }
+
+  Object.prototype.width = 0;
+
+  Object.prototype.height = 0;
+
+  return Object;
+
+})();
